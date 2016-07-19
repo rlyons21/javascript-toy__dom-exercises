79b867cbaacf72d23df7edbccd08cd249fc5540c
Rachel Lyons <rlyons21@hotmail.com>
2016-07-18 21:21:38 -0500

    completed exercise 5

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,17 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+	var button = document.getElementById("toggle_button");
+
+	button.addEventListener("click", function(){
+		var toggleMe = document.querySelectorAll("ul.second_five li");
+			if(toggleMe[0].style.display == "block"){
+				for(var i=0; i<toggleMe.length; i++){
+					toggleMe[i].style.display = "none";}
+				}
+			else {for(var i=0; i<toggleMe.length; i++){
+					toggleMe[i].style.display = "block";}
+
+			}
+	});
+
+});
