ad73572e67c1e3e38b324282ec576cc912193db5
Rachel Lyons <rlyons21@hotmail.com>
2016-07-19 11:40:43 -0500

    completed exercise 6

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,9 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+	var name = document.getElementById("full_name");
+
+	name.addEventListener("keyup", function(){
+		var greeting = document.getElementById("greeting");
+		greeting.innerHTML = ("Hello, " + name.value + "!");
+	});
+
+});
