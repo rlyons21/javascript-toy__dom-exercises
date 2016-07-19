2c1d7222e92831b8cbd7fec49ed63845f9a29158
Rachel Lyons <rlyons21@hotmail.com>
2016-07-19 13:44:02 -0500

    completed exercise 7

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here.
+window.addEventListener("load",function(){
+
+	var first = document.getElementById("first");
+	var second = document.getElementById("second");
+	var third = document.getElementById("third");
+
+	first.addEventListener("keyup", function(){
+		typed = first.value.length;
+		max = parseInt(first.getAttribute("maxlength"));
+
+		if(typed === max){
+			second.focus();
+		}
+	});
+
+	second.addEventListener("keyup", function(){
+		typed = second.value.length;
+		max = parseInt(second.getAttribute("maxlength"));
+
+		if(typed === max){
+			third.focus();
+		}
+	});
+});
